a614ecced993f976de2f16cf1306e99e2e8fc360
jackie <971060378@qq.com>
2016-12-17 03:47:04 +0800

    第一次提交！

---
+++ app/src/main/java/com/jackie/broadcast/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jackie.broadcast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/17.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
