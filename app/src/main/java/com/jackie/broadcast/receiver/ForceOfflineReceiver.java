e355bb7b0c81e136885075a0a7c325e8e6d4639b
jackie <971060378@qq.com>
2016-12-18 01:43:00 +0800

    12月17日进行的提交。实现强制下线的功能。

--- app/src/main/java/com/jackie/broadcast/receiver/ForceOfflineReceiver.java
+++ app/src/main/java/com/jackie/broadcast/receiver/ForceOfflineReceiver.java
@@ -22,17 +22,16 @@ public class ForceOfflineReceiver extends BroadcastReceiver {
         dialogBuilder.setTitle("Warning");
         dialogBuilder.setMessage("You are forced to be offline. Please try to login again.");
         dialogBuilder.setCancelable(false);
-        dialogBuilder.setPositiveButton("OK",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCollector.finishAll(); // 销毁所有活动
-                        Intent intent = new Intent(context,
-                                MainActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        context.startActivity(intent); // 重新启动LoginActivity
-                    }
-                });
+        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ActivityCollector.finishAll(); // 销毁所有活动
+                Intent intent = new Intent(context,
+                        MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent); // 重新启动LoginActivity
+            }
+        });
         AlertDialog alertDialog = dialogBuilder.create();
         // 需要设置AlertDialog的类型，保证在广播接收器中可以正常弹出
         alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
